55f4300d9854de9c66e04be7369830812352dbbf
Dennis Pettersson <mail@dennispettersson.se>
2018-10-23 16:44:59 +0200

    Added images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,49 @@
 import header from './images/header.png'
 import placeBackground from './images/place-background.png'
 
+import img0 from './images/places/hornan/P1070614-1.jpeg'
+import img1 from './images/places/hornan/P1070611-1.jpeg'
+import img2 from './images/places/torget/P1070549-1.jpeg'
+import img3 from './images/places/torget/P1070551-1.jpeg'
+import img4 from './images/places/torget/P1070584-1.jpeg'
+import img5 from './images/places/torget/P1070587-1.jpeg'
+import img6 from './images/places/torget/P1070553-1.jpeg'
+import img7 from './images/places/torget/P1070536-1.jpeg'
+import img8 from './images/places/torget/P1070537-1.jpeg'
+import img9 from './images/places/baksidan-passagen/IMG_4884.jpeg'
+import img10 from './images/places/baksidan-passagen/IMG_4885.jpeg'
+import img11 from './images/places/baksidan-passagen/IMG_2209.jpeg'
+import img12 from './images/places/baksidan-passagen/P1070603-1.jpeg'
+import img13 from './images/places/baksidan-passagen/IMG_2210.jpeg'
+import img14 from './images/places/centrum/IMG_4880.jpeg'
+import img15 from './images/places/centrum/IMG_4871.jpeg'
+import img16 from './images/places/centrum/P1070593-1.jpeg'
+import img17 from './images/places/centrum/P1070594-1.jpeg'
+import img18 from './images/places/centrum/IMG_4872.jpeg'
+import img19 from './images/places/centrum/IMG_4867.jpeg'
+
+
 export default [
   header,
   placeBackground,
+  img0,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
 ]
