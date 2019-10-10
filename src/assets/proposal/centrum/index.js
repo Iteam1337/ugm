cd51ad75119f1831c999aae9186945b88a148dd2
faroutchris <georgievchristo@gmail.com>
2019-10-10 14:11:28 +0200

    proposal data

---
+++ src/assets/proposal/centrum/index.js
@@ -0,0 +1,31 @@
+export default {
+    data: {
+        space: "Fittja Centrum",
+        recipient: "Land owner Kungsvåningen (private actor)",
+
+        observations: {
+            title: "Observations",
+            text: "Today Fittja Centrum mall is an unsafe, drug dealing area, where the stores do not offer interesting products to younger and female populations."
+        },
+        vision: {
+            title: "Vision",
+            text: "Fittja Centrum mall should be a bright space with good lighting and colour, that encourages commercial meeting points which gives all kinds of people a reason to stay and spend some time there."
+        },
+        possibilities: {
+            title: "Possibilities",
+            text: "Fittja Centrum mall really has the potential to provide places for women, children, and elderly to loiter, creating a space for more mixed use and more mixed populations, and also good lighting and colour."
+        },
+        priorities: {
+            title: "Design priorities",
+            text: "The design priorities are cafés, food courts, lighting, and new entrances and exits."
+        },
+        solution: {
+            title: "Main solution",
+            text: "We have chosen to focus on improving and developing the existing entrances, rather than adding more. Our vision is to create a welcoming and safe environment when visiting the mall."
+        },
+        action: {
+            title: "Action",
+            text: "To succeed, we must rebuild the main entrance with a new sliding door part and a curtain heater(no lock), create more space inside the mall at the entrance, look over side entrances, designing a new mall sign and new lightning."
+        }
+    }
+}
